e5cede62c6a5d15f785a6e802c93a9077d42dda8
vijay <vijayarora325@gmail.com>
2022-10-26 11:38:00 +0200

    Valid parentheses solution updated

--- base/stack.go
+++ base/stack.go
@@ -4,7 +4,7 @@ type Stack struct {
 	Items []rune
 }
 
-func (s *Stack) isEmpty() bool {
+func (s *Stack) IsEmpty() bool {
 	return len(s.Items) == 0
 }
 
@@ -13,11 +13,21 @@ func (s *Stack) Push(c rune) {
 }
 
 func (s *Stack) Pop() (rune, bool) {
-	if s.isEmpty() {
+	if s.IsEmpty() {
 		return 0, false
 	}
 	index := len(s.Items) - 1
 	value := s.Items[index]
 	s.Items = s.Items[:index]
 	return value, true
+}
+
+func (s *Stack) Top() rune {
+	if s.IsEmpty() {
+		return -1
+	}
+
+	index := len(s.Items) - 1
+	value := s.Items[index]
+	return value
 }
--- valid_parentheses.go
+++ valid_parentheses.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/vijaykarora/data-structure-problems/base"
+)
 
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+*/
+
+func main() {
+	s := "(])"
+	fmt.Println(isValid(s))
+}
+
+func isValid(s string) bool {
+	stack := base.Stack{}
+	arr := []rune(s)
+	for i := 0; i < len(arr); i++ {
+		current := arr[i]
+
+		if stack.IsEmpty() && (current == 41 || current == 125 || current == 93) {
+			stack.Push(current)
+			continue
+		}
+
+		if current == 40 || current == 123 || current == 91 {
+			stack.Push(current)
+			continue
+		}
+
+		top := stack.Top()
+		if current == 41 && top == 40 {
+			stack.Pop()
+			continue
+		}
+
+		if current == 125 && top == 123 {
+			stack.Pop()
+			continue
+		}
+
+		if current == 93 && top == 91 {
+			stack.Pop()
+			continue
+		}
+
+		stack.Push(current)
+	}
+	return stack.IsEmpty()
 }
